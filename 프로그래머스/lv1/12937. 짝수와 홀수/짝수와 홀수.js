2948b588486b98c3f233e59e2985194a844a1467
Moong <109510367+M-Moong@users.noreply.github.com>
2023-07-05 17:06:37 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.js
@@ -0,0 +1,12 @@
+function solution(num) {
+    // return num % 2 == 1 ? "Odd" : "Even";
+    
+    
+    if ( num % 2 == 1) {
+        return "Odd";
+    } else if ( num % 2 == 0){
+        return "Even";
+    } else {
+        return "Odd";
+    }
+}
