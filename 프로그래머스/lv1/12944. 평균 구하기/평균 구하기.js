43356b56017ed1885db52e1e6c084bcbdfd1f5a8
Moong <109510367+M-Moong@users.noreply.github.com>
2023-07-05 17:31:53 +0900

    [level 1] Title: 평균 구하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.js
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.js
@@ -1,4 +1,12 @@
 function solution(arr) {
+    // reduce 함수를 알고 푼 방식
     const answer = arr.reduce((a,b) => (a+b));
     return answer / arr.length;
+    
+    // for문을 돌리는 방식
+    // let sum;
+    // for (let i = 0; i < arr.length; i++) {
+    //     sum += arr[i];
+    // }
+    // return sum/arr.length;
 }
