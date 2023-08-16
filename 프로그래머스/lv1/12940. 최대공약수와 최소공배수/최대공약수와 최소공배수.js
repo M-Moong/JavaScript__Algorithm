4539bef77868b5f03dfcf2bc60d93ebecaa51d20
Moong <109510367+M-Moong@users.noreply.github.com>
2023-08-16 22:36:47 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 6.85 ms, Memory: 36.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -0,0 +1,22 @@
+function solution(n, m) {
+    // 최대공약수와 최소공배수를 반환하는 함수
+    // 배열 맨앞은 최대 공약수
+    // 그다음은 최소공배수
+    let gcd = 1;
+    for (let i = 2; i<=Math.min(n,m); i++) {
+        if(n % i === 0 && m % i === 0) {
+            gcd = i
+        }
+    }
+    console.log(gcd)
+    
+    let lcm = 1;
+    while(true){
+      if((lcm % n === 0) && (lcm % m === 0)){
+        break;
+      }
+      lcm++;
+    }
+  	console.log(lcm)
+    return [gcd, lcm]
+}
