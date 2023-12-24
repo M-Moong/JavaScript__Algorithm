ba557beb469cd2b77e74d47f46986fe5542b368d
Moong <109510367+M-Moong@users.noreply.github.com>
2023-12-24 12:23:18 +0900

    [level unrated] Title: 수열과 구간 쿼리 3, Time: 0.40 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
@@ -1,21 +1,6 @@
 function solution(arr, queries) {
-    let i = 0;
-    let j = 0;
-    let tempI = 0;
-    let tempJ = 0;
-
-    return queries.reduce((acc, cur, idx) => {
-        i = cur[0];
-        j = cur[1];
-
-        tempI = arr[i];
-        tempJ = arr[j];
-
-        acc[i] = tempJ;
-        acc[j] = tempI;
-
-        // console.log(tempI, tempJ, acc);
-
-        return acc;
-    }, arr);
+    queries.forEach( ([a,b]) => {
+        [arr[a],arr[b]] = [arr[b],arr[a]];
+    })
+    return arr;
 }
