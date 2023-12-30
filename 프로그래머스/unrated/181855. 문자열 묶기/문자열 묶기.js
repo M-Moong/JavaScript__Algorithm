79a60f991647e4967126a4225f41fc428ffa3d5c
Moong <109510367+M-Moong@users.noreply.github.com>
2023-12-30 12:21:29 +0900

    [level unrated] Title: 문자열 묶기, Time: 5.34 ms, Memory: 53.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181855. 문자열 묶기/문자열 묶기.js
@@ -0,0 +1,14 @@
+function solution(strArr) {
+    const answer = strArr.reduce((acc, cur) => {
+        const length = cur.length;
+
+        acc.set(length, acc.get(length) ? acc.get(length) + 1 : 1);
+
+        return acc;
+    }, new Map());
+
+
+    return Math.max(
+        ...[...answer.values()]
+    );
+}
