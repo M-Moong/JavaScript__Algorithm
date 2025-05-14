a50b3e2ab1828de53f1432d3db50e3e460bf395d
Moong <109510367+M-Moong@users.noreply.github.com>
2025-05-14 20:50:34 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 53.92 ms, Memory: 64.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -7,30 +7,52 @@ function solution(participant, completion) {
     //       return !completion.includes(runner)
     // }).join('')
     
-    const runMap = new Map()
+//     const runMap = new Map()
     
-    participant.map((runner) => {
-        if(!runMap.get(runner)) {
-            runMap.set(runner, 1)
-        } else {
-            runMap.set(runner, runMap.get(runner) + 1)
+//     participant.map((runner) => {
+//         if(!runMap.get(runner)) {
+//             runMap.set(runner, 1)
+//         } else {
+//             runMap.set(runner, runMap.get(runner) + 1)
+//         }
+//     })
+    
+//     completion.map((finisher) => {
+//         if(runMap.get(finisher)) {
+//             runMap.set(finisher,runMap.get(finisher) -1)
+//         } 
+//     })
+    
+//     let array = []
+    
+//     for (const key of runMap.keys()) {
+//         if(runMap.get(key) > 0) {
+//             array.push(key)
+//         }
+//     } 
+    
+//     return array.join('')
+
+////////////////////////////////////////////////////////////////////////////    
+    
+    const runMap = new Map();
+
+    // 1. 참가자 명단 돌면서 이름 개수 세기
+    participant.forEach(runner => {
+        runMap.set(runner, (runMap.get(runner) || 0) + 1);
+    });
+
+    // 2. 완주자 명단 돌면서 이름 개수 빼기
+    completion.forEach(finisher => {
+        runMap.set(finisher, runMap.get(finisher) - 1);
+    });
+
+    // 3. Map 순회하며 개수가 0이 아닌 선수 찾아서 바로 반환
+    for (const [runner, count] of runMap.entries()) {
+        if (count > 0) { 
+            return runner; 
         }
-    })
-    
-    completion.map((finisher) => {
-        if(runMap.get(finisher)) {
-            runMap.set(finisher,runMap.get(finisher) -1)
-        } 
-    })
-    
-    let array = []
-    
-    for (const key of runMap.keys()) {
-        if(runMap.get(key) > 0) {
-            array.push(key)
-        }
-    } 
-    
-    return array.join('')
-    
+    }
+
+    return '';
 }
