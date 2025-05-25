e3ffb371bb0e7482f9c530dd49276b08853f5648
Moong <109510367+M-Moong@users.noreply.github.com>
2025-05-25 18:04:48 +0900

    [level 2] Title: 의상, Time: 0.26 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,29 +1,19 @@
-function solution(clothes) {
-    // 코니는 매일 다른 옷 조합을 좋아한다.
-    
+function solution(clothes) {    
     const typeCount = {};
 
-for (const [item, type] of clothes) {
-  if (typeCount[type]) {
-    typeCount[type] += 1;
-  } else {
-    typeCount[type] = 1;
-  }
-}
-    
-    console.log(typeCount)
+    for (const [item, type] of clothes) {
+      if (typeCount[type]) {
+        typeCount[type] += 1;
+      } else {
+        typeCount[type] = 1;
+      }
+    }
     
     let answer = 1;
 
-for (const type in typeCount) {
+    for (const type in typeCount) {
+      answer *= (typeCount[type] + 1);
+    }
     
-  answer *= (typeCount[type] + 1);
-}
-
-answer -= 1; // 아무것도 안 입는 경우 제외
-    
-    return answer
-
-    
-    
+    return answer - 1; // 아무것도 안 입는 경우 제외
 }
