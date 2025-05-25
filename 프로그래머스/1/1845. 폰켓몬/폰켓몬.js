16b64fef41745f4eac7b7f8f77b860638595dba4
Moong <109510367+M-Moong@users.noreply.github.com>
2025-05-25 16:39:17 +0900

    [level 1] Title: 폰켓몬, Time: 0.55 ms, Memory: 34.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -0,0 +1,24 @@
+function solution(nums) {
+    // 폰켓몬 잡기위해 연구실 도착했다.
+    // 연구실에 있는 N마리 폰켓몬중 N/2마리를 가져가도 좋다.
+    // 폰켓몬 종류에 따라 번호를 붙인다.
+    // 같은 종류의 폰켓몬은 같은 번호를 가진다.
+    
+    // 예시 폰켓몬 : 4마리
+    // [3번, 1번, 2번, 3번]
+    // 31,32,33,12,13,23
+    
+    const possibleCount = nums.length/2
+    
+    const uniqueNumbers = new Set(nums)
+    
+    const aa = uniqueNumbers.size
+    
+    
+    if(aa <= possibleCount) {
+        return aa
+    } else {
+        return possibleCount
+    }
+
+}
